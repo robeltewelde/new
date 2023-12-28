a928718a42ee2cd82e8b9b96011fd2abbe54580b
robeltewelde <149680946+robeltewelde@users.noreply.github.com>
2023-12-28 15:06:34 +0300

    add

---
+++ new.js
@@ -0,0 +1,17 @@
+// confirmation
+// let age = "lets give me your age" ;
+// let  = Number(window.prompt("age"))
+// if (age >= 18) {
+//     console.log("You are an adult.");
+//  }
+// 0        1   2
+let array = [23, 24, 45];
+
+// for (let i = 0; i < 3; i++){
+//     console.log("Elements:",array[i] + array[i] * 0.1);
+// }
+let i = 0;
+while (i < array.length) {
+  console.log("Elements:", array[i] + array[i] * 0.1);
+  i++;
+}
--- te.js
+++ te.js
@@ -1,5 +1,64 @@
-const a = Number(window.prompt("give m a number: "));
-const b = Number(window.prompt("give m a number: "));
-const c = (a + b);
+// let password = 1234;
+// let counter = 0
 
-window.alert(c);
+//  while (true) {
+//     let pass_input = Number( window.prompt("Enter password"))
+//     if (pass_input === password && counter < 3) {
+//         alert ("Correct Password!")
+//     } else if (pass_input == "") {
+//         alert ("Please enter password")
+//         continue
+//     } else {
+//         alert ("Wrong password!!")
+//         counter++
+//         continue;
+//     }
+
+//     break;
+
+//  }
+
+//  window.alert("wellcome!!")
+//
+//              0   1      2
+// let array = ["Rob", 23, "cmc"];
+
+// let nam = "5+4";
+// let num1 = Number(nam[0]);
+// let num2 = Number(nam[2]);
+// let op = nam[1];
+
+// console.log(num1, num2, op); 25+36
+
+// while (true) {
+//   let entry = Number(window.prompt("5+4"))
+//   console.log(entry)
+//   let num1 = Number(entry[0]);
+//   let num2 = Number(entry[2]);
+//   let op = entry[1];
+  
+
+//   if (op == "+") {
+//     let sum = eval(entry);
+//     console.log(sum);
+//     continue;
+//   } else if (op == "/") {
+//     let div = num1 / num2;
+//     alert("Division: - ", div);
+//     continue;
+//   } else if (op == "*") {
+//     let mult = num1 * num2;
+//     alert("Product: - ", mult);
+//     continue;
+//   } else if (op == "-") {
+//     let diff = num1 - num2;
+//     alert("Difference: -", diff);
+//     continue;
+//   } else if (entry == "") {
+//     alert("Thank you for using my app");
+//     break;
+//   } else {
+//     alert("Invalid operator! Please use '+' , '-' , '/' or '*'. ");
+//     continue;
+//   }
+// }
