e3ac401ea18456970db0a4fa539e8be8b77f4a9e
robeltewelde <149680946+robeltewelde@users.noreply.github.com>
2024-01-01 16:05:18 +0300

    a

--- new.js
+++ new.js
@@ -5,13 +5,35 @@
 //     console.log("You are an adult.");
 //  }
 // 0        1   2
-let array = [23, 24, 45];
+// let array = [23, 24, 45];
 
 // for (let i = 0; i < 3; i++){
 //     console.log("Elements:",array[i] + array[i] * 0.1);
 // }
-let i = 0;
-while (i < array.length) {
-  console.log("Elements:", array[i] + array[i] * 0.1);
-  i++;
+// let i = 0;
+// while (i < array.length) {
+//   console.log("Elements:", array[i] + array[i] * 0.1);
+//   i++;
+// }
+
+// let array = [1, 2, 3, 4, 5];
+
+// array[5] = 20
+// for (let i = 0; i < 5; i++){
+//     console.log("elements:", array[i] + array[i] * 1)
+ //    
+// let array1 = []
+// for (let i = 0; i <= 50; i++){
+//     if (i % 2 == 0){
+//         array1.push(i)
+//     }
+// }
+// console.log(array1)
+
+let array = []
+for (let i = 0; i <= 50; i++){
+    if (i % 2 == 0) {
+        array.push(i)
+        }
 }
+console.log(array);
